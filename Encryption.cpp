99261146435d4edfb999ed737b9ab1d42be39291
Beran Çağlı <148129395+BeranCagli@users.noreply.github.com>
2024-12-20 12:59:27 +0300

    Update Encryption.cpp

--- Encryption.cpp
+++ Encryption.cpp
@@ -3,40 +3,51 @@
 
 using namespace std;
 
-class Encryption {
-public:
-	int encrypt(int password) {
-		int a, b, c, d;
-		d = password % 10 + 7;
-		password = password / 10;
-		c = password % 10 + 7;
-		password = password / 10;
-		b = password % 10 + 7;
-		password = password / 10;
-		a = password + 7;
 
-
-		d = d % 10;
-		c = c % 10;
-		b = b % 10;
-		a = a % 10;
+int Encryption::encrypt(int password) {
+	
+	if (password > 9999 || password < 0) {
+		cout << "Please enter 4 digit password..." << endl;
+		return -1;
+	}
 		
-		return c * 1000 + d * 100 + a * 10 + b;
-
-	}
-	int decyrpt(int password) {		
-		int a, b, c, d;
-		d = password % 10 + 3;
-		password = password / 10;
-		c = password % 10 + 3;
-		password = password / 10;
-		b = password % 10 + 3;
-		password = password / 10;
-		a = password % 10 + 3;
-
-		return a * 1000 + b * 100 + c * 10 + d;
-
-	}
+	int a, b, c, d;
+	d = password % 10 + 7;
+	password = password / 10;
+	c = password % 10 + 7;
+	password = password / 10;
+	b = password % 10 + 7;
+	password = password / 10;
+	a = password + 7;
 
 
-};
+	d = d % 10;
+	c = c % 10;
+	b = b % 10;
+	a = a % 10;
+		
+	return c * 1000 + d * 100 + a * 10 + b;
+
+}
+
+int Encryption::decrypt(int password) {		
+	
+	int a, b, c, d;
+	d = password % 10 + 3;
+	password = password / 10;
+	c = password % 10 + 3;
+	password = password / 10;
+	b = password % 10 + 3;
+	password = password / 10;
+	a = password % 10 + 3;
+
+	d = d % 10;
+	c = c % 10;
+	b = b % 10;
+	a = a % 10;
+
+	return c * 1000 + d * 100 + a * 10 + b;
+
+}
+
+
