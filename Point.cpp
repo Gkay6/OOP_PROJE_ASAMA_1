748a54f0db227e6ec70d018bdbb8184584599358
merzkan <113693032+merzkan1@users.noreply.github.com>
2024-12-20 19:24:40 +0300

    Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,39 +1,39 @@
-#include "POINT.h"
+#include "Point.h"
 
 // Getter function
-double POINT::getX() const {
+double Point::getX() const {
     return x;
 }
 
-double POINT::getY() const {
+double Point::getY() const {
     return y;
 }
 
 // Setter function
-void POINT::setX(double value) {
+void Point::setX(double value) {
     x = value;
 }
 
-void POINT::setY(double value) {
+void Point::setY(double value) {
     y = value;
 }
 
-void POINT::setPoint(double _x, double _y) {
+void Point::setPoint(double _x, double _y) {
     x = _x;
     y = _y;
 }
 
 // Calculate distance at two point
-double POINT::findDistanceToPos(const POINT& p) const {
+double Point::findDistanceToPos(const Point& p) const {
     return std::sqrt(std::pow(p.x - x, 2) + std::pow(p.y - y, 2));
 }
 
 // Decalculates the angle between two points (in radians)
-double POINT::findAngleToPos(const POINT& p) const {
+double Point::findAngleToPos(const Point& p) const {
     return std::atan2(p.y - y, p.x - x);
 }
 
 // Operator overload: Equality control
-bool POINT::operator==(const POINT& p) const {
+bool Point::operator==(const Point& p) const {
     return (x == p.x) && (y == p.y);
 }
