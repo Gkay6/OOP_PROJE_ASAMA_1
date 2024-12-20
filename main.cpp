9eb1e0af49c61d22a91650ff430d4e8c1eac843e
Gkay6 <gokaytaspinar12@gmail.com>
2024-12-21 00:00:47 +0300

    Main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "FestoRobotAPI.h"
+#include "Menu.h"
+#include <iostream>
+
+FestoRobotAPI* robot;
+
+// main function
+int main() {
+	//Create a robot object
+	robot = new FestoRobotAPI();
+
+
+	Menu robot_control_menu(robot);
+
+
+	delete robot;
+	return 0;
+}
