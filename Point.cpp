2533337b1b32e7ec5bc7b6ebcb1a2781e2eda017
merzkan <113693032+merzkan1@users.noreply.github.com>
2024-12-21 18:16:58 +0300

    Point.cpp
    With Doxygen

--- Point.cpp
+++ Point.cpp
@@ -1,39 +1,80 @@
 #include "Point.h"
 
-// Getter function
+/*!
+ * @brief Returns the X coordinate.
+ *
+ * @return The value of the X coordinate.
+ */
 double Point::getX() const {
     return x;
 }
 
+/*!
+ * @brief Returns the Y coordinate.
+ *
+ * @return The value of the Y coordinate.
+ */
 double Point::getY() const {
     return y;
 }
 
-// Setter function
+/*!
+ * @brief Sets the X coordinate.
+ *
+ * @param value: The new value for the X coordinate.
+ */
 void Point::setX(double value) {
     x = value;
 }
 
+/*!
+ * @brief Sets the Y coordinate.
+ *
+ * @param value: The new value for the Y coordinate.
+ */
 void Point::setY(double value) {
     y = value;
 }
 
+/*!
+ * @brief Sets both the X and Y coordinates.
+ *
+ * @param _x: The new value for the X coordinate.
+ * @param _y: The new value for the Y coordinate.
+ */
 void Point::setPoint(double _x, double _y) {
     x = _x;
     y = _y;
 }
 
-// Calculate distance at two point
+/*!
+ * @brief Calculates the distance between two points.
+ *
+ * @param p: The target point.
+ * @return The distance value.
+ */
 double Point::findDistanceToPos(const Point& p) const {
     return std::sqrt(std::pow(p.x - x, 2) + std::pow(p.y - y, 2));
 }
 
-// Decalculates the angle between two points (in radians)
+/*!
+ * @brief Calculates the angle between two points (in radians).
+ *
+ * @param p: The target point.
+ * @return The angle value (in radians).
+ */
 double Point::findAngleToPos(const Point& p) const {
     return std::atan2(p.y - y, p.x - x);
 }
 
-// Operator overload: Equality control
+/*!
+ * @brief Checks the equality of two points.
+ *
+ * @param p: The point to compare with.
+ * @return A boolean value indicating equality.
+ * @retval true: If the points are equal.
+ * @retval false: If the points are not equal.
+ */
 bool Point::operator==(const Point& p) const {
     return (x == p.x) && (y == p.y);
 }
