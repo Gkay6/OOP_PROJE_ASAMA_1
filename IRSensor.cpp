d555c1065a3c2f81807ced3dc27843bbc37ba8f7
Gkay6 <gokaytaspinar12@gmail.com>
2024-12-21 16:55:54 +0300

    IRSensor

--- IRSensor.cpp
+++ IRSensor.cpp
@@ -1,28 +1,37 @@
 #include "IRSensor.h"
 #include <iostream>
 
+
+// Constructor
 IRSensor::IRSensor(FestoRobotAPI* _robotAPI){
 	robotAPI = _robotAPI;
 
+	// Initial sensor values
 	for (int i = 0; i < 9; i++) {
-		ranges[i] = robotAPI->getIRRange(i) * CONVERSION_FACTOR;
+		ranges[i] = INT_MAX;
 	}
 }
 
-void IRSensor::update(int index) {
-	//Read IR range values
 
+// Updates the sensor at the given index, if no index is given updates all the sensors.
+void IRSensor::update(int index) {
+	
 	if (index == -1) {
+		// Update all sensors
 		for (int i = 0; i < 9; i++) {
 			ranges[i] = robotAPI->getIRRange(i) * CONVERSION_FACTOR;
 		}
 	}
 	else if (index >= 0 && index < 9) {
+		// Update single sensor
 		ranges[index] = robotAPI->getIRRange(index) * CONVERSION_FACTOR;
 	}
 }
 
+
+// Returns last IR sensor reading at given index
 double IRSensor::getRange(int index) {
+	
 	if (index >= 0 && index < 9) {
 		return ranges[index];
 	}
@@ -31,7 +40,10 @@ double IRSensor::getRange(int index) {
 	}
 }
 
+
+// [] operator overload, returns last IR sensor reading at given index
 double& IRSensor::operator[](int index) {
+	
 	if (index >= 0 && index < 9) {
 		return ranges[index];
 	}
--- IRSensor.h
+++ IRSensor.h
@@ -2,16 +2,30 @@
 #define IRSENSOR_H
 #include "FestoRobotAPI.h"
 
+
 class IRSensor
 {
 private:
-	double ranges[9];
-	FestoRobotAPI* robotAPI;
-	const double CONVERSION_FACTOR = 4.7619;
+	double ranges[9]; // Stores sensor values
+	FestoRobotAPI* robotAPI; // Robot API
+	
+	// FestoRobotAPI returns incorrect distance values
+	// The value is used to convert the API values to actual distance
+	// This value is specifically calculated for the sensor SharpGP2Y0A02YK0F
+	// And it is most accurate at 0.50 meters
+	const double CONVERSION_FACTOR = 4.8275;
 public:
+
+	// Constructor
 	IRSensor(FestoRobotAPI* robotAPI);
+	
+	// Updates the sensor at the given index, if no index is given updates all the sensors.
 	void update(int index = -1);
+
+	// Returns last IR sensor reading at given index
 	double getRange(int index);
+	
+	// [] operator overload, returns last IR sensor reading at given index
 	double& operator[](int index);
 };
 
