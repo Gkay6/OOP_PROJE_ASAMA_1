2cd10e80f9dcd9438bae9a18dc225c3ee0913027
merzkan <113693032+merzkan1@users.noreply.github.com>
2024-12-28 22:43:56 +0300

    RobotInterface.cpp

--- RobotInterface.cpp
+++ RobotInterface.cpp
@@ -1,1 +1,10 @@
+/** 
+ * @file   RobotInterface.cpp
+ * @author Omer Ozkan
+ * @date   December, 2024
+ * @brief  RobotInterface.cpp
+ *
+ * This file created for it was produced only for the regularization of the folder.
+ */
+
 #include "RobotInterface.h"
